eec8f5b427402127dab04df365c5c4f7d3718e44
tomas alvarado matamoros <tomas9610alvarado@gmail.com>
2020-12-13 12:51:28 -0600

    Este es mi primer repositorio en Git para un Socket

--- lib/config.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-date_default_timezone_set('America/Los_Angeles');
-
-define('DB_HOST', 'localhost');
-define('DB_USER', 'root');
-define('DB_PASS', '');
-define('DB_NAME', 'php-mysql-chat');
